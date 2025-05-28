945f0acd2ffe707a0e3b0077097e4f7d7b242cd3
marcop135 <marcop135.github@gmail.com>
2025-05-28 10:12:11 +0200

    fix: add missing package for vite

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = /*html*/`
-  <h1 class="text-3xl font-bold">Hellooooo world!</h1>
-`
+document.querySelector("#app").innerHTML = /*html*/ `
+  <h1 class="text-3xl font-bold font">Hellooooo world!</h1>
+`;
