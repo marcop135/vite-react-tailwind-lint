3fb58e664ba0e7c0f65e764af0b914ed7d9950a7
marcop135 <marcop135.github@gmail.com>
2025-06-03 16:28:31 +0200

    chore: update eslint and prettier configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,21 @@
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
+import parser from '@typescript-eslint/parser';
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    ignores: ['node_modules/', 'src/assets/js/vendor/'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: globals.browser,
+      parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     rules: {
       semi: ['error', 'always'],
